3d4dd67d60c869e07ea3be226ecb6dc3d5a5227f
Coolbrother <coolbrother@sourcegod.com>
2023-02-18 13:16:03 +0100

    Adding: (change_bpm in MainApp class, and params in main function for commandline Interactive)

--- src/audinotes.py
+++ src/audinotes.py
@@ -131,6 +131,19 @@ class MainApp(object):
 
     #-------------------------------------------
 
+    def change_bpm(self, bpm, adding=0):
+        if self.mixer is None: return
+        bpm = float(bpm)
+        cur_bpm = self.player.get_bpm()
+        if adding == 1: # is incremental
+            bpm += cur_bpm
+
+        cur_bpm = self.player.set_bpm(bpm)
+        msg = f"Bpm: {cur_bpm}"
+        self.display(msg)
+
+    #-------------------------------------------
+
 
     def main(self, input_device_index, output_device_index):
         self.init_app(input_device_index, output_device_index)
@@ -138,126 +151,142 @@ class MainApp(object):
         
         self.display(msg)
         sav_str = ''
-        while 1:
-            val_str = input("-> ")
-            if val_str == '': val_str = sav_str
-            else: sav_str = val_str
 
-            if val_str in ('q', 'Q'):
-                self.player.close()
-                self.display("Quit...")
-                break
+        try:
+            while 1:
 
-            elif val_str == 'b': # Forward
-                pos = self.player.forward()
-                pos = self.player.samples_to_sec(pos)
-                msg = f"Time at: {pos:.3f} Secs"
-                self.display(msg)
-            elif val_str in (' ', 't', 'p'):
-                if not self.player.is_playing():
-                    msg = "Play"
-                    self.player.play()
+                key = param1 = param2 = param3 = None
+                val_str = input("-> ")
+                if val_str == '': val_str = sav_str
+                else: sav_str = val_str
+
+                if val_str == " ":
+                    key = val_str
                 else:
-                    self.player.pause()
+                    lst = val_str.split()
+                    len_lst = len(lst)
+                    if len_lst >0: key = lst[0]
+                    if len_lst >1: param1 = lst[1]
+                    if len_lst >2: param2 = lst[2]
+                    if len_lst >3: param3 = lst[3]
+
+                
+                if key in ('q', 'Q'):
+                    self.player.close()
+                    self.display("Quit...")
+                    break
+
+                elif key == 'b': # Forward
+                    pos = self.player.forward()
+                    pos = self.player.samples_to_sec(pos)
+                    msg = f"Time at: {pos:.3f} Secs"
+                    self.display(msg)
+                elif key in (' ', 't', 'p'):
+                    if not self.player.is_playing():
+                        msg = "Play"
+                        self.player.play()
+                    else:
+                        self.player.pause()
+                        pos = self.player.get_position()
+                        pos = self.player.samples_to_sec(pos) # convert to sec
+                        msg = f"Pause at: {pos:.3f} secs"
+                    self.display(msg)
+                elif key == 'k':
+                    val = self.player.toggle_click()
+                    if val: msg = "Start Clicking"
+                    else: msg = "Stop Clicking"
+                    self.display(msg)
+                elif key == 'l':
+                    val = self.player.toggle_loop()
+                    if val: msg = "Start Looping"
+                    else: msg = "Stop Looping"
+                    self.display(msg)
+                elif key == 'r':
+                    # toggle record
+                    val = self.player.toggle_record()
+                    if val: msg = "Start Record"
+                    else: msg = "Stop Record"
+                    self.display(msg)
+
+                elif key == 'R':
+                    # toggle record mode
+                    val = self.player.toggle_rec_mode()
+                    if val == 0:
+                        msg = "Record mode Replace"
+                    else:
+                        msg = "Record mode Mix"
+                    self.display(msg)
+                elif key == 'v':
+                    self.player.stop()
                     pos = self.player.get_position()
-                    pos = self.player.samples_to_sec(pos) # convert to sec
-                    msg = f"Pause at: {pos:.3f} secs"
-                self.display(msg)
-            elif val_str == 'k':
-                val = self.player.toggle_click()
-                if val: msg = "Start Clicking"
-                else: msg = "Stop Clicking"
-                self.display(msg)
-            elif val_str == 'l':
-                val = self.player.toggle_loop()
-                if val: msg = "Start Looping"
-                else: msg = "Stop Looping"
-                self.display(msg)
-            elif val_str == 'r':
-                # toggle record
-                val = self.player.toggle_record()
-                if val: msg = "Start Record"
-                else: msg = "Stop Record"
-                self.display(msg)
+                    pos = self.player.samples_to_sec(pos)
+                    msg = f"Stop at: {pos:.3f} Secs"
+                    self.display(msg)
+                elif key == 'w': # Rewind
+                    pos = self.player.rewind()
+                    pos = self.player.samples_to_sec(pos)
+                    msg = f"Time at: {pos:.3f} Secs"
+                    self.display(msg)
+                elif key == 'x':
+                    val = self.player.toggle_mute()
+                    if val: msg = "Muted On"
+                    else: msg = "Muted Off"
+                    self.display(msg)
+                elif key == 'z': # wiring
+                    self.player.recwire()
+                elif key == '<': # goto start
+                    pos = self.player.goto_start()
+                    pos = self.player.samples_to_sec(pos)
+                    msg = f"Goto Start at: {pos:.3f} Secs"
+                    self.display(msg)
+                elif key == '>': # goto End
+                    pos = self.player.goto_end()
+                    pos = self.player.samples_to_sec(pos)
+                    msg = f"Goto End at: {pos:.3f} Secs"
+                    self.display(msg)
+                elif key in ('?', 'h',):
+                    self.display(_help)
 
-            elif val_str == 'R':
-                # toggle record mode
-                val = self.player.toggle_rec_mode()
-                if val == 0:
-                    msg = "Record mode Replace"
+                elif key == 'bpm':
+                    # change bpm
+                    if not param1: param1 = 120
+                    self.change_bpm(param1, adding=0) # not incremental
+                    
+                elif key == 'bpmd':
+                    # Dec bpm
+                    if not param1: param1 =-10
+                    self.change_bpm(param1, adding=1)
+
+                elif key == 'bpmi':
+                    # inc bpm
+                    if not param1: param1 =10
+                    self.change_bpm(param1, adding=1)
+
+                elif key == 'dev':
+                    self.print_devices()
+                elif key == 'engsta':
+                    self.start_engine()
+                elif key == 'engsto':
+                    self.stop_engine()
+                elif key == 'init':
+                    self.player.init_track()
+                elif  key in ('sta', 'status'): # Status
+                    state = self.player.get_state()
+                    pos = self.player.get_position()
+                    pos = self.player.samples_to_sec(pos)
+                    # start_loop = self.player.get_start_loop()
+                    # end_loop = self.player.get_end_loop()
+                    msg = f"{state}, Position: {pos:.3f} Secs"
+                    self.display(msg)
+                elif key == 'test':
+                    self.test()
+                 
                 else:
-                    msg = "Record mode Mix"
-                self.display(msg)
-            elif val_str == 'v':
-                self.player.stop()
-                pos = self.player.get_position()
-                pos = self.player.samples_to_sec(pos)
-                msg = f"Stop at: {pos:.3f} Secs"
-                self.display(msg)
-            elif val_str == 'w': # Rewind
-                pos = self.player.rewind()
-                pos = self.player.samples_to_sec(pos)
-                msg = f"Time at: {pos:.3f} Secs"
-                self.display(msg)
-            elif val_str == 'x':
-                val = self.player.toggle_mute()
-                if val: msg = "Muted On"
-                else: msg = "Muted Off"
-                self.display(msg)
-            elif val_str == 'z': # wiring
-                self.player.recwire()
-            elif val_str == '<': # goto start
-                pos = self.player.goto_start()
-                pos = self.player.samples_to_sec(pos)
-                msg = f"Goto Start at: {pos:.3f} Secs"
-                self.display(msg)
-            elif val_str == '>': # goto End
-                pos = self.player.goto_end()
-                pos = self.player.samples_to_sec(pos)
-                msg = f"Goto End at: {pos:.3f} Secs"
-                self.display(msg)
-            elif val_str in ('?', 'h',):
-                self.display(_help)
-
-            elif val_str == 'bpm':
-                # Set bpm
-                bpm = self.player.set_bpm(120)
-                msg = f"Set bpm: {bpm}"
-                self.display(msg)
-            elif val_str == 'bpmd':
-                # Dec bpm
-                bpm = self.player.get_bpm()
-                bpm = self.player.set_bpm(bpm - 10)
-                msg = f"Dec bpm: {bpm}"
-                self.display(msg)
-            elif val_str == 'bpmi':
-                # inc bpm
-                bpm = self.player.get_bpm()
-                bpm = self.player.set_bpm(bpm + 10)
-                msg = f"Inc bpm: {bpm}"
-                self.display(msg)
-            elif val_str == 'dev':
-                self.print_devices()
-            elif val_str == 'engsta':
-                self.start_engine()
-            elif val_str == 'engsto':
-                self.stop_engine()
-            elif val_str == 'init':
-                self.player.init_track()
-            elif  val_str in ('sta', 'status'): # Status
-                state = self.player.get_state()
-                pos = self.player.get_position()
-                pos = self.player.samples_to_sec(pos)
-                # start_loop = self.player.get_start_loop()
-                # end_loop = self.player.get_end_loop()
-                msg = f"{state}, Position: {pos:.3f} Secs"
-                self.display(msg)
-            elif val_str == 'test':
-                self.test()
-             
-            else:
-                msg = "Command not found"
-                self.display(msg)
+                    msg = "Command not found"
+                    self.display(msg)
+        finally:
+            # save history file
+            pass
 
     #----------------------------------------
 
